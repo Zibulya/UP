e26d9c7f5634f22ac5c6166e7f7aefa2d8c45bd6
Zibulya <maxzibulya@rambler.ru>
2023-03-22 10:26:03 +0300

    test
    test

---
+++ Программа/arraysort/ArraySorter/QuickSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sizoy_QuickSort
+{
+    public class QuickSortet
+    {
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+     
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        public static int[] SortArray(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+
+    }
+}
---
+++ Программа/arraysort/ArraySorter/Resources/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Sizoy_QuickSort;
+using Sizoy_SwapSort;
+using Sizoy_CoctailSort;
+using Sizoy_InsertionSort;
+namespace ArraySorter
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string myArr = textBox4.Text;
+            int[] result = myArr.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+            string texter = string.Join("",CoctailSorter.SortArray(result));
+            textBox3.Text = texter;
+            sw.Stop();
+            textBox7.Text = sw.Elapsed.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                textBox1.Text = "";
+                MessageBox.Show(
+        "Неверный тип данных",
+        "Ошибка",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1);
+
+            }
+
+            int[] x = new int[n];
+            for (int i = 0; i < x.Length; i++)
+            {
+                textBox4.Text = textBox4.Text + x[i];
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string myArr = textBox4.Text;
+            int[] result = myArr.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+            string texter = string.Join("", SwapSorter.SortArray(result));
+            textBox3.Text = texter;
+            sw.Stop();
+            textBox6.Text = sw.Elapsed.ToString();
+        }
+
+      
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            int[] x = new int[n];
+            if (x.Length > textBox4.TextLength)
+            {
+                int a = x.Length - textBox4.TextLength;
+                while (a > 0)
+                {
+                    a--;
+                    textBox4.Text = textBox4.Text + 0;
+                }
+            }
+            else
+            if (x.Length < textBox4.TextLength)
+            {
+                int a = textBox4.TextLength - x.Length;
+
+                textBox4.Text = textBox4.Text.Remove(textBox4.Text.Length - a);
+                a = 0;
+
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string myArr = textBox4.Text;
+            int[] result = myArr.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+            int resl = result.Length; 
+            string texter = string.Join("", InsertionSorter.SortArray(result));
+            sw.Stop();
+            textBox5.Text = sw.Elapsed.ToString();
+            textBox3.Text = texter;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string myArr = textBox4.Text;
+            int[] result = myArr.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
+            int resl = result.Length;
+            string texter = string.Join("", QuickSortet.SortArray(result));
+            sw.Stop();
+            textBox2.Text = sw.Elapsed.ToString();
+            textBox3.Text = texter;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
